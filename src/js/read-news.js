b1f2c3d3bb336b327db793a85ec90735df8793eb
Alex <103459735+torn80beta@users.noreply.github.com>
2023-03-27 17:19:20 +0300

    it's working!!!

--- src/js/read-news.js
+++ src/js/read-news.js
@@ -2,17 +2,79 @@ import { ReadStorage } from './localStorage';
 
 const readStorage = new ReadStorage();
 const news = readStorage.getNews();
-console.log(`News: `);
-console.log(news);
 
-// Раскоментить это
+// News array for test
+// const news = [
+//   {
+//     alt: 'Politics and Government, Speeches and Statements',
+//     info: 'Protests broke out shortly after Prime Minister Benjamin Netanyahu fired the defense minister, who had called for a halt to efforts to weaken the judiciary.',
+//     published_date: '2023-03-26',
+//     readDate: '27/03/2023',
+//     section: 'World',
+//     src: 'https://static01.nyt.com/images/2023/03/26/multimedia/26israel-new-mwcq-promo/26israel-new-mwcq-mediumThreeByTwo440.jpg',
+//     title: 'Israel Boils as Netanyahu Ousts Minister Who Bucked Court Overhaul',
+//     url: 'https://www.nytimes.com/2023/03/26/world/middleeast/judiciary-overhaul-benjamin-netanyahu-israel-parliament.html',
+//   },
+//   {
+//     alt: 'Politics and Government, Speeches and Statements',
+//     info: 'Protests broke out shortly after Prime Minister Benjamin Netanyahu fired the defense minister, who had called for a halt to efforts to weaken the judiciary.',
+//     published_date: '2023-03-26',
+//     readDate: '27/03/2023',
+//     section: 'World',
+//     src: 'https://static01.nyt.com/images/2023/03/26/multimedia/26israel-new-mwcq-promo/26israel-new-mwcq-mediumThreeByTwo440.jpg',
+//     title: 'Israel Boils as Netanyahu Ousts Minister Who Bucked Court Overhaul',
+//     url: 'https://www.nytimes.com/2023/03/26/world/middleeast/judiciary-overhaul-benjamin-netanyahu-israel-parliament.html',
+//   },
+//   {
+//     alt: 'Politics and Government, Speeches and Statements',
+//     info: 'Protests broke out shortly after Prime Minister Benjamin Netanyahu fired the defense minister, who had called for a halt to efforts to weaken the judiciary.',
+//     published_date: '2023-03-26',
+//     readDate: '26/03/2023',
+//     section: 'World',
+//     src: 'https://static01.nyt.com/images/2023/03/26/multimedia/26israel-new-mwcq-promo/26israel-new-mwcq-mediumThreeByTwo440.jpg',
+//     title: 'Israel Boils as Netanyahu Ousts Minister Who Bucked Court Overhaul',
+//     url: 'https://www.nytimes.com/2023/03/26/world/middleeast/judiciary-overhaul-benjamin-netanyahu-israel-parliament.html',
+//   },
+//   {
+//     alt: 'Politics and Government, Speeches and Statements',
+//     info: 'Protests broke out shortly after Prime Minister Benjamin Netanyahu fired the defense minister, who had called for a halt to efforts to weaken the judiciary.',
+//     published_date: '2023-03-26',
+//     readDate: '26/03/2023',
+//     section: 'World',
+//     src: 'https://static01.nyt.com/images/2023/03/26/multimedia/26israel-new-mwcq-promo/26israel-new-mwcq-mediumThreeByTwo440.jpg',
+//     title: 'Israel Boils as Netanyahu Ousts Minister Who Bucked Court Overhaul',
+//     url: 'https://www.nytimes.com/2023/03/26/world/middleeast/judiciary-overhaul-benjamin-netanyahu-israel-parliament.html',
+//   },
+//   {
+//     alt: 'Politics and Government, Speeches and Statements',
+//     info: 'Protests broke out shortly after Prime Minister Benjamin Netanyahu fired the defense minister, who had called for a halt to efforts to weaken the judiciary.',
+//     published_date: '2023-03-26',
+//     readDate: '25/03/2023',
+//     section: 'World',
+//     src: 'https://static01.nyt.com/images/2023/03/26/multimedia/26israel-new-mwcq-promo/26israel-new-mwcq-mediumThreeByTwo440.jpg',
+//     title: 'Israel Boils as Netanyahu Ousts Minister Who Bucked Court Overhaul',
+//     url: 'https://www.nytimes.com/2023/03/26/world/middleeast/judiciary-overhaul-benjamin-netanyahu-israel-parliament.html',
+//   },
+//   {
+//     alt: 'Politics and Government, Speeches and Statements',
+//     info: 'Protests broke out shortly after Prime Minister Benjamin Netanyahu fired the defense minister, who had called for a halt to efforts to weaken the judiciary.',
+//     published_date: '2023-03-26',
+//     readDate: '25/03/2023',
+//     section: 'World',
+//     src: 'https://static01.nyt.com/images/2023/03/26/multimedia/26israel-new-mwcq-promo/26israel-new-mwcq-mediumThreeByTwo440.jpg',
+//     title: 'Israel Boils as Netanyahu Ousts Minister Who Bucked Court Overhaul',
+//     url: 'https://www.nytimes.com/2023/03/26/world/middleeast/judiciary-overhaul-benjamin-netanyahu-israel-parliament.html',
+//   },
+// ];
+
+// Uncomment this
 // const dates = [...new Set(news.map(obj => obj.readDate))];
 
-// Закоментить это
+// Comment this
 dates = ['27/03/2023', '26/03/2023', '25/03/2023'];
 
 const accordionEl = document.querySelector('.accordion');
-console.log('Dates: ' + dates);
+// console.log('Dates: ' + dates);
 
 function renderAccordion() {
   const markup = dates
@@ -30,49 +92,37 @@ function renderAccordion() {
   return markup;
 }
 
-/*  */
-
-// function renderFromStorage(news) {
-//   const markup = news
-//     .map(({ src, title, url, info, published_date, alt, section }) => {
-//       return `<li class="card-photo">
-//                 <div class="image-wrapper">
-//           <img class="photo" src="https://www.nytimes.com/${src}" alt="${alt}" loading="lazy" />
-//           </div>
-//               <div class="card-category">${section}</div>
-//               <button type="button" class="favorite-button__activ">Remove from favorite</button>
-//               <h2 class="card-title">${title}</h2>
-//               <p class="card-info">${info}</p>
-//               <span class="card-date">${published_date}</span>
-//               <a href="${url}" class="card-url">Read more</a>
-
-//               </li>`;
-//     })
-//     .join('');
-
-//   //   favGallery.innerHTML = markup;
-// }
-
-/*  */
-
 accordionEl.innerHTML = renderAccordion();
-
 const accordion = document.getElementsByClassName('container');
-
 const labels = document.querySelectorAll('.accordion-date');
 
 // console.log(labels);
+// { readDate, src, title, url, info, published_date, alt, section }
+console.log(`News: `);
+console.log(news);
 
 labels.forEach(element => {
-  //   console.log(element.innerHTML);
+  const accordionElDate = element.innerText;
+  console.log(`Checking for ${accordionElDate}`);
   news.map(
     ({ readDate, src, title, url, info, published_date, alt, section }) => {
-      console.log('Reading news');
-      if (readDate === element.innerText) {
-        console.log('Hooraay!!!');
+      // console.log(el);
+      // const readDate = el.readDate;
+      // const src = el.src;
+      // const title = el.title;
+      // const url = el.url;
+      // const info = el.info;
+      // const published_date = el.published_date;
+      // const alt = el.alt;
+      // const section = el.section;
+      // console.log('Reading news date: ' + readDate);
+      if (readDate === accordionElDate) {
+        //   console.log(readDate === accordionElDate);
+        //   console.log(readDate);
+        //   console.log('Hooraay!!!');
         const markup = `<li class="card-photo">
                 <div class="image-wrapper">
-          <img class="photo" src="https://www.nytimes.com/${src}" alt="${alt}" loading="lazy" />
+          <img class="photo" src="${src}" alt="${alt}" loading="lazy" />
           </div>
               <div class="card-category">${section}</div>
               <button type="button" class="favorite-button__activ">Remove from favorite</button>
