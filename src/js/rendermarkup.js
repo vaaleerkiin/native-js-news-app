f428f5c5c775bb47e334e114f585de68b6842dd3
HusarOlha <husarolya@gmail.com>
2023-03-27 17:28:30 +0300

    add icon and notification

--- src/js/rendermarkup.js
+++ src/js/rendermarkup.js
@@ -52,14 +52,17 @@ function renderMarkup(searchedNews) {
           }
 
           return `<li class="card-photo">
-    //   		<div class="image-wrapper">
-    //     <img class="photo" src="${image()}" alt="${keyword()}" loading="lazy" />
-		// </div>
+      		<div class="image-wrapper">
+         <img class="photo" src="${image()}" alt="${keyword()}" loading="lazy" />
+		 </div>
             <div class="card-category">${section_name}</div>
             <button type="button" class="${activeClass}">Add to favorite
-             <svg class="item-news__block-icon active-news-icon" width="16" height="16" viewBox="0 0 37 32">
-                              <path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.2857" d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z"></path>
-                      </svg></span></button>
+            <span><svg class="item-news__block-icon active-news-icon" width="16" height="16" viewBox="0 0 37 32">
+    <path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4"
+        stroke-width="2.2857"
+        d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z">
+    </path>
+</svg></span></button>
             <h2 class="card-title">${headline.main}</h2>
             <p class="card-info">${abstract}</p>
             <span class="card-date">${cardDate}</span>
@@ -121,14 +124,19 @@ function onCardClick(e) {
         'favorite-button__activ',
         'add-to-favorite'
       );
-      favoriteBtn.textContent = 'Add to favorite';
+      // favoriteBtn.textContent = 'Add to favorite';
+      favoriteBtn.innerHTML =
+        'Add to favorite <svg class=" active-news-icon" width="16" height="16" viewBox="0 0 37 32"><path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.2857" d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z"></path></svg>';
     } else {
       favoriteStorage.addNews(data);
       favoriteBtn.classList.replace(
         'add-to-favorite',
         'favorite-button__activ'
       );
-      favoriteBtn.textContent = 'Remove from favorite';
+      // favoriteBtn.textContent = 'Remove from favorite ';
+      // favoriteBtn.insertAdjacentHTML('beforeend', '');
+      favoriteBtn.innerHTML =
+        'Remove from favorite  <svg class="news-icon" width="16" height="16" viewBox="0 0 37 32"><path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.2857" d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z"></path></svg>';
     }
     return data;
   }
@@ -147,6 +155,10 @@ function onReadClick(e) {
     console.log(newsCard);
     newsCard.classList.add('opacity');
     const headline = newsCard.querySelector('.card-title').textContent;
+    newsCard.insertAdjacentHTML(
+      'beforeend',
+      '<span class="news__read-status">Already read</span>'
+    );
 
     const multimediaSrc = newsCard.querySelector('img').getAttribute('src');
     const multimediaAlt = newsCard.querySelector('img').getAttribute('alt');
